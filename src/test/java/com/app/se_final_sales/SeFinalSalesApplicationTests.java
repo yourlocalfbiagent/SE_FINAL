1c1c301d5bb63b2b638e1f3e26d251198096510f
Christofe Sayah <christophesayah12@gmail.com>
2026-04-30 21:29:32 +0300

    Initialized Spring Boot Project

---
+++ src/test/java/com/app/se_final_sales/SeFinalSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.se_final_sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeFinalSalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
