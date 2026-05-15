50f304385bc0ff6d3529b5edf7867c2e3012bfd4
Christofe Sayah <christophesayah12@gmail.com>
2026-05-15 04:34:45 +0300

    ci(erp): trigger pipeline test

--- src/main/java/com/sefinal/erp/ErpApplication.java
+++ src/main/java/com/sefinal/erp/ErpApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = {"com.sefinal.erp", "com.app.se_final_sales"})
 @EntityScan(basePackages = {"com.sefinal.erp", "com.app.se_final_sales"})
 public class ErpApplication {
+    // Trigger CI/CD Pipeline Test
     public static void main(String[] args) {
         SpringApplication.run(ErpApplication.class, args);
     }
